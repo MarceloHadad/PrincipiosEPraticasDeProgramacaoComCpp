a47b1d729b76ba60b57fbd975bacf13085706ee3
Marcelo Hadad <marcelo.hadad@intelitrader.com.br>
2022-03-23 01:52:35 -0300

    Exibe duas linhas para atender item 1 do Issue

--- Capitulo_2/Pratica/oi_mundo/oi_mundo/oi_mundo.cpp
+++ Capitulo_2/Pratica/oi_mundo/oi_mundo/oi_mundo.cpp
@@ -1,7 +1,9 @@
 #include "std_lib_facilities.h"
 int main() // O programa C++ inicia executando a função main
 {
-	cout << "Oi, Mundo!\n"; // escreve “Oi, Mundo!” na saída
+	cout << "Oi, programação!\n";
+	cout << "Aqui vamos nós!\n";
+
 	keep_window_open(); // aguarda a entrada de um caractere
 	return 0;
 }
