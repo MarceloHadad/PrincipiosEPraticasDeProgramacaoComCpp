7f795831588a0bc34d49e2701ebccf7b24178b3f
Marcelo Hadad <marcelo.hadad@intelitrader.com.br>
2022-03-26 11:42:43 -0300

    Adiciona nova estrutura do repo, alteração do exercicio #1 e adiciona exercicio #2

---
+++ instructions/instructions.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "A casa tem dois andares, no andar de baixo ficam a sala de estar, sala de jantar, cozinha e um lavabo\n";
+    cout << "No andar de cima, ficam dois quartos e um banheiro\n";
+    cout << "Se você estiver sentado em uma cadeira, levante da cadeira\n";
+    cout << "Vá em direção ao corredor, após 5 passos, vire à direita\n";
+    cout << "Vá em direção à escada, suba até o último degrau\n";
+    cout << "Vire à esquerda e encontrará a porta do banheiro\n";
+    return 0;
+}
---
+++ oi_mundo/oi_mundo.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+int main() // O programa C++ inicia executando a fun��o main
+{
+	cout << "Oi, programação!\n";
+	cout << "Aqui vamos nós!\n";
+
+	keep_window_open(); // aguarda a entrada de um caractere
+	return 0;
+}
