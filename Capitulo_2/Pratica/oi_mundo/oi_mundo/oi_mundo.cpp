9de00ad817ad948ec8fcc8e804d3134221215b84
Marcelo Hadad <marcelo.hadad@intelitrader.com.br>
2022-03-26 11:39:45 -0300

    Deleta estrutura antiga do repo

--- Capitulo_2/Pratica/oi_mundo/oi_mundo/oi_mundo.cpp
+++
@@ -1,9 +0,0 @@
-#include "std_lib_facilities.h"
-int main() // O programa C++ inicia executando a função main
-{
-	cout << "Oi, programação!\n";
-	cout << "Aqui vamos nós!\n";
-
-	keep_window_open(); // aguarda a entrada de um caractere
-	return 0;
-}
