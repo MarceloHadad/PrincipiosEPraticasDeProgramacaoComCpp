83abca007e34786d835008be66523ec0157b7b4e
Marcelo Hadad <marcelo.hadad@intelitrader.com.br>
2022-03-28 13:40:07 -0300

    Altera instrução, considerando o exemplo do livro.

--- instructions/instructions.cpp
+++ instructions/instructions.cpp
@@ -4,7 +4,8 @@ int main()
 {
     cout << "A casa tem dois andares, no andar de baixo ficam a sala de estar, sala de jantar, cozinha e um lavabo\n";
     cout << "No andar de cima, ficam dois quartos e um banheiro\n";
-    cout << "Se você estiver sentado em uma cadeira, levante da cadeira\n";
+    // No §2.1 o exemplo utilizado é de que a pessoa está sentada à mesa.
+    cout << "Você está sentado à mesa, então levante da cadeira\n";
     cout << "Vá em direção ao corredor, após 5 passos, vire à direita\n";
     cout << "Vá em direção à escada, suba até o último degrau\n";
     cout << "Vire à esquerda e encontrará a porta do banheiro\n";
