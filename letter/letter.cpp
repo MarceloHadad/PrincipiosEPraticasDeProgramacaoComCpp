6a300a3fb253d09ac4df76c0387a3e465050a902
Marcelo Hadad <marcelo.hadad@outlook.com>
2022-03-30 00:48:06 -0300

    Altera mensagem de solicitação inicial e itens necessários para atender o item 3 do Issue.

--- letter/letter.cpp
+++ letter/letter.cpp
@@ -2,10 +2,13 @@
 
 int main()
 {
-	cout << "Digite o nome da pessoa para quem deseja escrever\n";
+	cout << "Digite o nome da pessoa para quem deseja escrever:\n";
 	string primeiro_nome;
 	cin >> primeiro_nome;
 	cout << "Prezado(a) " << primeiro_nome << ",\n";
 	cout << "Como você vai? Eu estou bem.\n";
 	cout << "Sinto sua falta. Já faz muito tempo que não nos falamos.\n";
+	cout << "Digite o nome de outro amigo:\n";
+	string nome_amigo;
+	cout << "Você viu " << nome_amigo << " recentemente?\n";
 }
