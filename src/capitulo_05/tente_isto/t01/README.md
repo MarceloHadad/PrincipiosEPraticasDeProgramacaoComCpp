daabea94759532ffa7b4a17c27f8ba2dd1d270cf
Marcelo Hadad <marcelo.hadad@outlook.com>
2023-01-05 13:27:18 -0300

    #18: Implements the t1 program.

---
+++ src/capitulo_05/tente_isto/t01/README.md
@@ -0,0 +1,3 @@
+# Tente Isto #1
+
+Tente compilar aqueles exemplos e veja como o compilador responde.
---
+++ src/capitulo_05/tente_isto/t01/c05t01.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	int area(int lenght, int width);
+
+	int s1 = area(7;
+	int s2 = area(7)
+	Int s3 = area(7);
+	int s4 = area('7);
+
+	return 0;
+}
