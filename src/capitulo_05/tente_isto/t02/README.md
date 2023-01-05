30d605292e78db203ae38304e2289ebc8fc364e4
Marcelo Hadad <marcelo.hadad@outlook.com>
2023-01-05 13:49:54 -0300

    #18: Implements the t2 program.

---
+++ src/capitulo_05/tente_isto/t02/README.md
@@ -0,0 +1,3 @@
+# Tente Isto #2
+
+Tente compilar aqueles exemplos e veja como o compilador responde. Tente pensar em mais alguns erros e teste com eles.
---
+++ src/capitulo_05/tente_isto/t02/c05t02.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	int area(int lenght, int width);
+
+	int x0 = arena(7);
+	int x1 = area(7);
+	int x2 = area("seven", 2);
+	int x3 = area("7, 2");
+
+	return 0;
+}
